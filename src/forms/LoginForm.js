a05c999a29e9781a65a9f989a0acc0c54fc74348
Jason Hutchinson <jrh150730@utdallas.edu>
2018-06-14 21:00:52 -0500

    added verification to HomePage UI text fields

--- src/forms/LoginForm.js
+++ src/forms/LoginForm.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {Form, Button} from 'semantic-ui-react';
+import validator from "validator";
+import InlineError from '../messages/InlineError'
 class LoginForm extends React.Component
 {
     state={
@@ -13,13 +16,26 @@ class LoginForm extends React.Component
 
     onChange = e => this.setState({data:{...this.state.data, [e.target.name]: e.target.value}
     });
+    onSubmit = () =>{
+        const errors = this.validate(this.state.data);
+        this.setState({errors});
+        if(Object.key(errors).length === 0){
+            this.props.submit(this.state.data);
+        }
+    }
 
-    render()
+    validate = (data) => {
+        const errors = {};
+        if(!validator.isEmail(data.email)) errors.email = "invalid email";
+        if(!data.password) errors.password = "Can't be blank";
+        return errors;
+    }
+        render()
     {
-        const{data} = this.state;
+        const{data, errors} = this.state;
         return(
-            <Form>
-                <Form.Field>
+            <Form onSubmit={this.onSubmit}>
+                <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input
                         type={"email"}
@@ -29,8 +45,9 @@ class LoginForm extends React.Component
                         value={data.email}
                         onChange={this.onChange}
                     />
+                    {errors.email && <InlineError text={errors.email}/>}
                 </Form.Field>
-                <Form.Field>
+                <Form.Field error={!!errors.password}>
                     <label htmlFor="password">Password</label>
                     <input
                         type={"password"}
@@ -40,11 +57,14 @@ class LoginForm extends React.Component
                         value={data.password}
                         onChange={this.onChange}
                     />
+                    {errors.password && <InlineError text={errors.password}/>}
                 </Form.Field>
                 <Button primary>Login</Button>
             </Form>
         );
     }
 }
-
+LoginForm.propTypes = {
+    submit: PropTypes.func.isRequired
+}
 export default LoginForm
