df72f2f5c210567e89458765fb514b95402dbfd9
Jason Hutchinson <jrh150730@utdallas.edu>
2018-08-04 13:47:02 -0700

    Changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import HomePage from "./components/pages/HomePage/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import styles from "../src/App.css"
+import CreateAccountForm from "./forms/CreateAccountForm";
 
 const App = () => (
         <div className='uicontainer' style={{styles}}>
             <Route path="/" exact component={HomePage}/>
             <Route path="/login" exact component={LoginPage}/>
+            <Route path="/create" exact component={CreateAccountForm}/>
         </div>
 
 );
--- src/forms/CreateAccountForm.js
+++ src/forms/CreateAccountForm.js
@@ -1,9 +1,10 @@
 import * as firebase from 'firebase';
-import React from 'react';
+import React, {Component} from 'react';
 import styles from '../forms/FormStyles.css';
 import {Form, Button} from 'semantic-ui-react';
+import addImage from '../addImage.png';
 
-class CreateAccountForm extends Component() {
+class CreateAccountForm extends Component {
     state = {
         data: {
             firstName: "",
@@ -27,52 +28,76 @@ class CreateAccountForm extends Component() {
     render(){
         const {data} = this.state
         return (
-            <div className="createAccountContainer">
+            <div className="createAccountContainer" style={{styles}}>
                 <Form onSubmit={this.onSubmit()}>
-                    <Form.Field className='firstNameField'>
-                        <label htmlFor="firstName">First Name</label>
-                        <input
-                            type={"string"}
-                            id={"firstName"}
-                            name={"FirstName"}
-                            placeholder={"First Name"}
-                            value={data.firstName}
-                            onChange={this.onChange}
-                        />
-                    </Form.Field>
-                    <Form.Field className='lastNameField'>
-                        <label htmlFor="lastName">Last Name</label>
-                        <input
-                            type={"string"}
-                            id={"lastName"}
-                            name={"LastName"}
-                            placeholder={"Last Name"}
-                            value={data.lastName}
-                            onChange={this.onChange}
-                        />
-                    </Form.Field>
-                    <Form.Field className='emailField'>
-                        <label htmlFor="email">Last Name</label>
-                        <input
-                            type={"email"}
-                            id={"email"}
-                            name={"Email"}
-                            placeholder={"John.Doe@example.com"}
-                            value={data.email}
-                            onChange={this.onChange}
-                        />
-                    </Form.Field>
-                    <Form.Field className='passwordField'>
-                        <label htmlFor="password">Last Name</label>
-                        <input
-                            type={"password"}
-                            id={"password"}
-                            name={"Password"}
-                            placeholder={"password"}
-                            value={data.password}
-                            onChange={this.onChange}
-                        />
-                    </Form.Field>
+                    <div className='profilePicture'>
+                        <img className="ui small circular image" src={addImage}/>
+                    </div>
+                    <div className='generalInfo'>
+                        <div className='two fields'>
+                            <Form.Field className='firstNameField'>
+                                <label htmlFor="firstName">First Name</label>
+                                <input
+                                    type={"string"}
+                                    id={"firstName"}
+                                    name={"FirstName"}
+                                    placeholder={"First Name"}
+                                    value={data.firstName}
+                                    onChange={this.onChange}
+                                />
+                            </Form.Field>
+                            <Form.Field className='lastNameField'>
+                                <label htmlFor="lastName">Last Name</label>
+                                <input
+                                    type={"string"}
+                                    id={"lastName"}
+                                    name={"LastName"}
+                                    placeholder={"Last Name"}
+                                    value={data.lastName}
+                                    onChange={this.onChange}
+                                />
+                            </Form.Field>
+                        </div>
+                        <Form.Field className='emailField'>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type={"email"}
+                                id={"email"}
+                                name={"Email"}
+                                placeholder={"John.Doe@example.com"}
+                                value={data.email}
+                                onChange={this.onChange}
+                            />
+                        </Form.Field>
+                        <Form.Field className='passwordField'>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type={"password"}
+                                id={"password"}
+                                name={"Password"}
+                                placeholder={"password"}
+                                value={data.password}
+                                onChange={this.onChange}
+                            />
+                        </Form.Field>
+                        <Form.Field>
+                            <label>University</label>
+                            <select className='ui fluid dropdown'>
+                                <option value='The University of Texas At Dallas'>Univ. Texas at Dallas</option>
+                                <option value='The University of North Texas'>Univ. North Texas</option>
+                                <option value='Southern Methodist University'>Southern Methodist University</option>
+                            </select>
+                        </Form.Field>
+                    </div>
+                    <div className='addClassesView'>
+                        <div className="ui category search">
+                            <div className="ui icon input">
+                                <input className="prompt" type="text" placeholder="Search animals..."/>
+                                    <i className="search icon"/>
+                            </div>
+                            <div className="results"></div>
+                        </div>
+                    </div>
                 </Form>
             </div>
         )
