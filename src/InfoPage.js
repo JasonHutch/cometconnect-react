60b857eef3f6c76be75094e93848cf69710885e2
Jason Hutchinson <jrh150730@utdallas.edu>
2018-08-10 16:43:58 -0500

    Added info home page

--- src/InfoPage.js
+++ src/InfoPage.js
@@ -1,11 +1,26 @@
 import React, {Compnent} from 'react';
 import styles from '../src/App.css';
-class InfoPage extends Compnent {
+import infoHeader from '../src/infoHeader.jpg';
+import aboutPhoto from '../src/edwin-andrade-153753-unsplash.jpg'
+
+class InfoPage extends React.Component {
     render() {
-        return(
+        return (
             <div className='InfoPageContainer' style={{styles}}>
-                test
+                <div className='infoHeader'>
+                    <img className="ui fluid image" src={infoHeader}/>
+                </div>
+                <div className='infoAbout'>
+                    <img className="ui fluid image" src={aboutPhoto}/>
+                    <div className='aboutContent'>
+                        <p id='aboutUsText'>Comet Connect is a way for students
+                        to come together to learn, plan, and succeed more efficiently. As well as
+                        create a collaborative university environment that promotes collective problem solving</p>
+
+                    </div>
+                </div>
             </div>
+
         )
     }
 }
