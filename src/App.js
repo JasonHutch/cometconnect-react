8bfc72699a0614819c0d729cdd3fe2b7ea28d94f
Jason Hutchinson <jrh150730@utdallas.edu>
2018-08-10 13:46:39 -0500

    Added info page

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./components/pages/HomePage/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import styles from "../src/App.css"
 import CreateAccountForm from "./forms/CreateAccountForm";
+import InfoPage from "./InfoPage";
 
 const App = () => (
         <div className='uicontainer' style={{styles}}>
             <Route path="/" exact component={HomePage}/>
+            <Route path="/info" exact component={InfoPage}/>
             <Route path="/login" exact component={LoginPage}/>
             <Route path="/create" exact component={CreateAccountForm}/>
         </div>
---
+++ src/InfoPage.js
@@ -0,0 +1,13 @@
+import React, {Compnent} from 'react';
+import styles from '../src/App.css';
+class InfoPage extends Compnent {
+    render() {
+        return(
+            <div className='InfoPageContainer' style={{styles}}>
+                test
+            </div>
+        )
+    }
+}
+
+export default InfoPage
